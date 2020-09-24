6d7724aa9951065ab2a59c8809b521959e19f64f
jeongxhj <55428319+jeongxhj@users.noreply.github.com>
2020-09-24 16:44:54 +0900

    실습 06

--- main.c
+++ main.c
@@ -5,13 +5,15 @@
 
 int main(int argc, char *argv[]) {
 	int input;
+	int hour, sec, min;
 	
-	printf("input year : ");
+	printf("input sec : ");
+	scanf("%i", &input);
 	
-	scanf("%d", &input);
+	hour = input/3600;
+	min = (input%3600)/60;
+	sec = input%60;
 	
-	printf("is %d leap year? %d \n", input,
-									(input%4==0 && input%100!=0)||(input%400==0) );
-	
+	printf("the time is %i : %i : %i \n", hour, min, sec);
 	return 0;
 }
